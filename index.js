27f89b198dd9d20aadfaf5efa44d200b16bef16f
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-05-19 10:08:07 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// 1 завдання
+// const numbers = [1, 2, 3];
+// numbers.push(4, 5, 6);
+// console.log(numbers);
+
+// 2 завдання
+// const numbers = [1, 2, 3];
+// numbers.unshift(4, 5, 6)
+// console.log(numbers);
+
+// завдання 3
+// const lenth = ['js', 'css', 'jq'];
+// console.log(lenth.shift(0));
+
+// завдання 4
+// const lenth = ['js', 'css', 'jq'];
+// console.log(lenth.pop(2));
+
+// завдання 5
+// завдання 6
+// ?
+
+// завдання 8
+const styles = ['Джаз', 'Блюз']
+styles.push('Рок-н-ролл');
+styles[1] = 'Класика'
+styles.unshift('реп','рок')
+console.log(styles.shift(0))
+console.log(styles)
