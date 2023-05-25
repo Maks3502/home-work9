60f34e1105a984f6ac51a32cd82d1fd7a86f153c
Maks3502 <107855225+Maks3502@users.noreply.github.com>
2023-05-25 18:18:36 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -17,13 +17,25 @@
 // console.log(lenth.pop(2));
 
 // завдання 5
-// завдання 6
-// ?
+// const number = [1, 2, 3, 4];
+// number.splice(4, 0, 6, 7);
+// console.log(number);
+
+
+// завдання 7
+
+// const fruit = ["Banana", "apple", "orange", "lemon", "pear"];
+// fruit[0] = "peach";
+// fruit [4] = "apricot";
+// fruit.slice(2, 1, "plum");
+// console.log(alert(fruit));
+
+
 
 // завдання 8
-const styles = ['Джаз', 'Блюз']
-styles.push('Рок-н-ролл');
-styles[1] = 'Класика'
-styles.unshift('реп','рок')
-console.log(styles.shift(0))
-console.log(styles)
+// const styles = ['Джаз', 'Блюз']
+// styles.push('Рок-н-ролл');
+// styles[1] = 'Класика'
+// styles.unshift('реп','рок')
+// console.log(styles.shift(0))
+// console.log(styles)
